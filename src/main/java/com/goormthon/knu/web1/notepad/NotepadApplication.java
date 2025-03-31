c28fcac2b1245a7cd6fc98769d9cc41abf9df391
강예린 <yerin7786@gmail.com>
2025-03-31 22:40:38 +0900

    initial commit

---
+++ src/main/java/com/goormthon/knu/web1/notepad/NotepadApplication.java
@@ -0,0 +1,13 @@
+package com.goormthon.knu.web1.notepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepadApplication.class, args);
+	}
+
+}
