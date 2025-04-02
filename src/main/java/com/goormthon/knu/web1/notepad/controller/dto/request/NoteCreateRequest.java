a0a86525aa7462c95753208e07d14077ff28e723
강예린 <yerin7786@gmail.com>
2025-04-03 01:32:04 +0900

    feat: 생성자 어노테이션 추가 - 테스트 케이스 작성을 위해 @NoArgsConstructor, @AllArgsConstructor 추가

--- src/main/java/com/goormthon/knu/web1/notepad/controller/dto/request/NoteCreateRequest.java
+++ src/main/java/com/goormthon/knu/web1/notepad/controller/dto/request/NoteCreateRequest.java
@@ -1,9 +1,13 @@
 package com.goormthon.knu.web1.notepad.controller.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NoteCreateRequest {
 
     @NotBlank(message = "제목은 비어있을 수 없습니다.")
