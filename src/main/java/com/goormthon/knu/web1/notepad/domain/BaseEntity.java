50d6652a30209d70044034fa520c0c60d0c23f04
강예린 <yerin7786@gmail.com>
2025-04-03 17:59:13 +0900

    refactor: 어노테이션 추가

--- src/main/java/com/goormthon/knu/web1/notepad/domain/BaseEntity.java
+++ src/main/java/com/goormthon/knu/web1/notepad/domain/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -19,7 +20,10 @@ public class BaseEntity {
     private Long id;
 
     @CreatedDate
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
