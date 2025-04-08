cab8a6ec85d0b5808f21ce176a775b10325765b4
강예린 <yerin7786@gmail.com>
2025-04-08 23:10:34 +0900

    feat: CORS 설정

---
+++ src/main/java/com/goormthon/knu/web1/notepad/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.goormthon.knu.web1.notepad.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
